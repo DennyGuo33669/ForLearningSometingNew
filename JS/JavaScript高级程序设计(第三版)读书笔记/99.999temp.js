3979f60dc49d5f8354d3e998273e4a5989313784
DennyGuo_PC <dennyguo33669@gmail.com>
2019-07-01 17:50:37 +0800

    docs(js笔记):add 5.2.5

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,10 +1,1 @@
-var colors = new Array(); // 创建
-var count = colors.unshift('red', 'green'); // 推入两项
-console.log(count);
 
-count = colors.unshift('black'); // 推入另一项
-console.log(count);
-
-var item = colors.pop(); // 取得第一项
-console.log(item); // green
-console.log(colors.length);
