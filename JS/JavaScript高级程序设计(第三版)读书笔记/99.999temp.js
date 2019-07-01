8d6123ef044ea5cae8a4d23c5c6b2b4d099f5104
DennyGuo_PC <dennyguo33669@gmail.com>
2019-07-01 18:44:08 +0800

    docs(js笔记): add 5.2.6(unfinished)

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,1 +1,6 @@
+var colors = ['red', 'green', 'blue', 'yellow', 'purple'];
+var colors2 = colors.slice(1);
+var colors3 = colors.slice(1, 4);
 
+console.log(colors2); // 'green', 'blue', 'yellow', 'purple'
+console.log(colors3); // green', 'blue', 'yellow'
