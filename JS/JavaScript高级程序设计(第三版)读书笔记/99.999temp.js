a8c6d060b8484967bf231e855179f132e55b56a5
DennyGuo_PC <dennyguo33669@gmail.com>
2019-07-03 17:13:45 +0800

    docs(js笔记): add 5.2.7

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,12 +1,1 @@
-var colors = ['red', 'green', 'blue'];
-var removed = colors.splice(0, 1); // 删除第一项
-console.log(colors); // 'green', 'blue'
-console.log(removed); // 'red'
 
-removed = colors.splice(1, 0, 'yellow', 'orange');
-console.log(colors); // 'green', 'yellow', 'orange', 'blue'
-console.log(removed); // 返回一个空数组
-
-removed = colors.splice(1, 1, 'red', 'purple');
-console.log(colors); // 'green', 'red', 'purple', 'orange', 'blue'
-console.log(removed); // 'yellow'
