3d96640d8dee195268bfe7e8b1a19ce0433dcf63
DennyGuo_PC <dennyguo33669@gmail.com>
2019-07-09 17:23:20 +0800

    docs(js笔记): add 5.5.3

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,3 +1,22 @@
+function createComparisonFunction (propertyName) {
+  return function (object1, object2) {
+    var value1 = object1[propertyName];
+    var value2 = object2[propertyName];
 
+    if (value1 < value2) {
+      return -1;
+    } else if (value1 > value2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+}
 
+var data = [{ name: 'Jam', age: 28 }, { name: 'Emma', age: 29 }];
 
+data.sort(createComparisonFunction('name'));
+console.log(data[0].name); // "Emma"
+
+data.sort(createComparisonFunction('age'));
+console.log(data[0].name); // Jam
