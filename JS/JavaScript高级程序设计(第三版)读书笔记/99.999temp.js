6a49398eff22fb62bf36a77d70ea8df20b6bbcd4
DennyGuo_PC <dennyguo33669@gmail.com>
2019-07-16 19:27:50 +0800

    docs(js笔记): add 5.7.2

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,1 +1,8 @@
+function selectFrom (lowerValue, upperValue) {
+  var choices = upperValue - lowerValue;
+  return Math.floor(Math.random() * choices + lowerValue);
+}
 
+var colors = ['red', 'green', 'blue', 'yellow', 'black', 'purple', 'brown'];
+var color = colors[selectFrom(0, colors.length - 1)];
+console.log(color);
