59b8c5cf1d0e03042388eeda557fc0fae7f0dec6
DennyGuo_PC <dennyguo33669@gmail.com>
2019-08-16 15:21:47 +0800

    docs(English): add the English learning folder

---
+++ English/coach shane/Daily Easy English Expression.md
@@ -0,0 +1,1 @@
+# Daily Easy English Expression
--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,26 +1,19 @@
-
 function createComparisonFunction (propertyName) {
   return function (obj1, obj2) {
-    var value1 = obj1[ propertyName];
+    var value1 = obj1[propertyName];
     var value2 = obj2[propertyName];
-
-    if (value1 < value2) {
-      return -1;
-    } else if (value1 > value2) {
-      return 1;
-    } else {
-      return 0;
-    }
+    var value3 = JSON.stringify(arguments);
+    // if (value1 < value2) {
+    //   return -1;
+    // } else if (value1 > value2) {
+    //   return 1;
+    // } else {
+    //   return 0;
+    // }
+    console.log(`value1:${value1},value2:${value2},参数:${value3}`);
   };
 }
 
-// 创建函数
-var compareNames = createComparisonFunction('name');
+var data = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
 
-// 调用函数
-var result = compareNames({ name: 'Emma' }, { name: 'Jam' });
-
-// 解除对匿名函数的引用(以便释放内存)
-compareNames = null;
-
-console.log(result);
+data.sort(createComparisonFunction('name'));
