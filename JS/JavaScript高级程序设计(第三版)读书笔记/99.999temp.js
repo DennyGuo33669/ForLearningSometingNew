f3a31d1a143e5d65efb82e9e20e6c860aec4ede0
DennyGuo_Laptop_xiaomiPro <dennyguo33669@gmail.com>
2019-09-05 21:31:38 +0800

    docs(js笔记): add 7.2.2

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,23 +1,38 @@
-// function createComparisonFunction (propertyName) {
-//   return function (obj1, obj2) {
-//     var value1 = obj1[propertyName];
-//     var value2 = obj2[propertyName];
-//     var value3 = JSON.stringify(arguments);
-//     // if (value1 < value2) {
-//     //   return -1;
-//     // } else if (value1 > value2) {
-//     //   return 1;
-//     // } else {
-//     //   return 0;
-//     // }
-//     console.log(`value1:${value1},value2:${value2},参数:${value3}`);
-//   };
+// // function createComparisonFunction (propertyName) {
+// //   return function (obj1, obj2) {
+// //     var value1 = obj1[propertyName];
+// //     var value2 = obj2[propertyName];
+// //     var value3 = JSON.stringify(arguments);
+// //     // if (value1 < value2) {
+// //     //   return -1;
+// //     // } else if (value1 > value2) {
+// //     //   return 1;
+// //     // } else {
+// //     //   return 0;
+// //     // }
+// //     console.log(`value1:${value1},value2:${value2},参数:${value3}`);
+// //   };
+// // }
+
+// // var data = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+
+// // data.sort(createComparisonFunction('name'));
+
+// for (var i = 14; i < 100; i++) {
+//   console.log(`[^${i + 1}]:`);
 // }
 
-// var data = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+var name = 'The window';
 
-// data.sort(createComparisonFunction('name'));
+var obj = {
+  name: 'My object',
 
-for (var i = 14; i < 100; i++) {
-  console.log(`[^${i + 1}]:`);
-}
+  getNameFunc: function () {
+    var that = this;
+    return function () {
+      return that.name;
+    };
+  }
+};
+
+console.log(obj.getNameFunc()()); // 'My object'
