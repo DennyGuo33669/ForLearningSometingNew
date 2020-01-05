b9f67919667d110b89f3fa53ee5c6ea2350828be
DennyGuo_Laptop_xiaomiPro <dennyguo33669@gmail.com>
2020-01-06 01:31:18 +0800

    update test1

--- JS/test/查询.js
+++ JS/test/查询.js
@@ -1,13 +1,24 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/';
-
+let 分母 = 0;
+let 分子 = 0;
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
   if (err) throw err;
   var dbo = db.db('mycol');
-  var whereStr = { 成绩: 1 }; // 查询条件
-  dbo.collection('三江分数_郭子文1').find({}, { 成绩: 1, _id: 0 }).toArray(function (err, result) {
+  var whereStr = { }; // 查询条件
+  var colName = '三江分数_郭子文';
+
+  dbo.collection(colName).find(whereStr).toArray(function (err, result) {
     if (err) throw err;
-    console.log(result);
+
+    for (const key in result) {
+      分母 += result[key].学分 * result[key].绩点;
+      分子 += result[key].学分;
+    }
+
+    const 平均绩点 = 分母 / 分子;
+    console.log(`平均绩点为${平均绩点.toFixed(1)}`);
+
     db.close();
   });
 });
