b0d7df7aa600ba5581d1ccc647d19275ce90fd2d
DennyGuo_Laptop_xiaomiPro <dennyguo33669@gmail.com>
2020-01-07 22:46:35 +0800

    update

--- JS/node.js/test1/计算平均绩点.js
+++ JS/node.js/test1/计算平均绩点.js
@@ -1,14 +1,14 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/';
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
 let 分母 = 0;
 let 分子 = 0;
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
   if (err) throw err;
-  var dbo = db.db('mycol');
-  var whereStr = { }; // 查询条件
-  var colName = ['三江分数_郭子文', '三江分数_张秋晨'];
-
-  dbo.collection(colName[0]).find(whereStr).toArray(function (err, result) {
+  const dbo = db.db('mycol');
+  const whereStr = { }; // 查询条件
+  const colName = ['三江分数_郭子文', '三江分数_张秋晨'];
+  const who = colName[1];
+  dbo.collection(who).find(whereStr).toArray(function (err, result) {
     if (err) throw err;
 
     const 计算平均绩点 = (result) => {
@@ -19,8 +19,7 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
       const 平均绩点 = 分母 / 分子;
       return 平均绩点.toFixed(1);
     };
-
-    console.log(`平均绩点为${计算平均绩点(result)}`);
+    console.log(`${who.slice(5)}的平均绩点为${计算平均绩点(result)}`);
     db.close();
   });
 });
