6d7ca33f35493c91e7e6bb09e0576ab109d5a514
DennyGuo_PC <dennyguo33669@gmail.com>
2020-01-14 18:32:07 +0800

    8.2.1

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -109,8 +109,46 @@
 // // 取消
 // clearTimeout(timeoutId);
 
-// 显示 "打印" 对话框
-window.print();
+// // 显示 "打印" 对话框
+// window.print();
 
-// 显示 "查找" 对话框
-window.find();
+// // 显示 "查找" 对话框
+// window.find();
+
+function getQueryStringArgs () {
+  // 取得查询字符串并去掉开头的问号
+  var qs = (location.search.length > 0 ? location.search.substring(1) : '');
+
+  // 保存数据的对象
+  var args = {};
+
+  // 取得每一项
+  var items = qs.length ? qs.split('&') : [];
+  var item = null;
+  var name = null;
+  var value = null;
+
+  // 在for循环中使用
+  var i = 0;
+  var len = items.length;
+
+  // 逐个将每一项添加到args对象中
+  for (i = 0; i < len; i++) {
+    item = items[i].split('=');
+    name = decodeURIComponent(item[0]);
+    value = decodeURIComponent(item[1]);
+
+    if (name.length) {
+      args[name] = value;
+    }
+  }
+
+  return args;
+}
+
+// 假设查询字符串是?q=javascript&num=10
+
+var args = getQueryStringArgs();
+
+console.log(args["q"]); // "javascript"
+console.log(args["num"]); // "10"
