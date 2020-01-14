a4c93037b0cf7d8179143fe1c493fe204f9836f5
DennyGuo_PC <dennyguo33669@gmail.com>
2020-01-14 19:11:17 +0800

    8.2.2 unfinished

--- JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
+++ JS/JavaScript高级程序设计(第三版)读书笔记/99.999temp.js
@@ -1,154 +1,159 @@
 
-// // // // // function createComparisonFunction (propertyName) {
-// // // // //   return function (obj1, obj2) {
-// // // // //     var value1 = obj1[propertyName];
-// // // // //     var value2 = obj2[propertyName];
-// // // // //     var value3 = JSON.stringify(arguments);
-// // // // //     // if (value1 < value2) {
-// // // // //     //   return -1;
-// // // // //     // } else if (value1 > value2) {
-// // // // //     //   return 1;
-// // // // //     // } else {
-// // // // //     //   return 0;
-// // // // //     // }
-// // // // //     console.log(`value1:${value1},value2:${value2},参数:${value3}`);
-// // // // //   };
+// // // // // // function createComparisonFunction (propertyName) {
+// // // // // //   return function (obj1, obj2) {
+// // // // // //     var value1 = obj1[propertyName];
+// // // // // //     var value2 = obj2[propertyName];
+// // // // // //     var value3 = JSON.stringify(arguments);
+// // // // // //     // if (value1 < value2) {
+// // // // // //     //   return -1;
+// // // // // //     // } else if (value1 > value2) {
+// // // // // //     //   return 1;
+// // // // // //     // } else {
+// // // // // //     //   return 0;
+// // // // // //     // }
+// // // // // //     console.log(`value1:${value1},value2:${value2},参数:${value3}`);
+// // // // // //   };
+// // // // // // }
+
+// // // // // // var data = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+
+// // // // // // data.sort(createComparisonFunction('name'));
+
+// // // // // for (var i = 14; i < 100; i++) {
+// // // // //   console.log(`[^${i + 1}]:`);
 // // // // // }
 
-// // // // // var data = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+// // // // // var pageWidth = window.innerWidth;
+// // // // // var pageHeight = window.innerHeight;
 
-// // // // // data.sort(createComparisonFunction('name'));
-
-// // // // for (var i = 14; i < 100; i++) {
-// // // //   console.log(`[^${i + 1}]:`);
-// // // // }
-
-// // // // var pageWidth = window.innerWidth;
-// // // // var pageHeight = window.innerHeight;
-
-// // // // if (typeof pageWidth !== 'number') {
-// // // //   if (document.compatMode == 'CSS1Compat') {
-// // // //     pageWidth = document.documentElement.clientWidth;
-// // // //     pageHeight = document.documentElement.clientHeight;
-// // // //   } else {
-// // // //     pageWidth = document.body.clientWidth;
-// // // //     pageHeight = document.body.clientHeight;
+// // // // // if (typeof pageWidth !== 'number') {
+// // // // //   if (document.compatMode == 'CSS1Compat') {
+// // // // //     pageWidth = document.documentElement.clientWidth;
+// // // // //     pageHeight = document.documentElement.clientHeight;
+// // // // //   } else {
+// // // // //     pageWidth = document.body.clientWidth;
+// // // // //     pageHeight = document.body.clientHeight;
+// // // // //   }
+// // // // // }
+// // // // var app = new Vue({
+// // // //   el: '#app',
+// // // //   data: {
+// // // //     message: 'Hello Vue!'
 // // // //   }
-// // // // }
-// // // var app = new Vue({
-// // //   el: '#app',
-// // //   data: {
-// // //     message: 'Hello Vue!'
+// // // // });
+
+// // // // var app = new Vue({
+// // // //   el: '#app-2',
+// // // //   data: {
+// // // //     message: '页面加载于' + new Date().toLocaleString()
+// // // //   }
+// // // // });
+
+// // // var data = {
+// // //   use: false,
+// // //   url: 'https://www.bilibili.com',
+// // //   msg: '坎坎坷坷'
+// // // };
+// // // var vm = new Vue({
+// // //   el: '#example',
+// // //   data: data,
+// // //   methods: {
+// // //     reversedMsg: function () {
+// // //       this.msg = this.msg.split('').reverse().join('');
+// // //     }
+// // //   },
+// // //   filters: {
+// // //     capitalize: function (val) {
+// // //       if (!val) {
+// // //         return '';
+// // //       }
+// // //       val = val.toString();
+// // //       return val.charAt(0).toUpperCase() + val.slice(1);
+// // //     }
 // // //   }
 // // // });
+// // ;
 
-// // // var app = new Vue({
-// // //   el: '#app-2',
-// // //   data: {
-// // //     message: '页面加载于' + new Date().toLocaleString()
-// // //   }
-// // // });
+// // // // 调整大小
+// // // baiduWin.resizeTo(500, 500);
 
-// // var data = {
-// //   use: false,
-// //   url: 'https://www.bilibili.com',
-// //   msg: '坎坎坷坷'
-// // };
-// // var vm = new Vue({
-// //   el: '#example',
-// //   data: data,
-// //   methods: {
-// //     reversedMsg: function () {
-// //       this.msg = this.msg.split('').reverse().join('');
-// //     }
-// //   },
-// //   filters: {
-// //     capitalize: function (val) {
-// //       if (!val) {
-// //         return '';
-// //       }
-// //       val = val.toString();
-// //       return val.charAt(0).toUpperCase() + val.slice(1);
-// //     }
+// // // // 移动位置
+// // // baiduWin.moveTo(100, 100);
+
+// // // // 关闭新打开的窗口
+// // // baiduWin.close();
+
+// // // baiduWin.close();
+
+// // var blocked = false;
+
+// // try {
+// //   var baiduWin = window.open('http://www.baidu.com', '_blank');
+
+// //   if (baiduWin == null) {
+// //     blocked = true;
 // //   }
-// // });
-// ;
+// // } catch (ex) {
+// //   blocked = true;
+// // }
 
-// // // 调整大小
-// // baiduWin.resizeTo(500, 500);
+// // if (blocked) {
+// //   console.log('The popup was blocked!');
+// // }
 
-// // // 移动位置
-// // baiduWin.moveTo(100, 100);
+// // // 设置超时调用
+// // var timeoutId = setTimeout(() => {
+// //   alert('hell!');
+// // }, 1000);
 
-// // // 关闭新打开的窗口
-// // baiduWin.close();
+// // // 取消
+// // clearTimeout(timeoutId);
 
-// // baiduWin.close();
+// // // 显示 "打印" 对话框
+// // window.print();
 
-// var blocked = false;
+// // // 显示 "查找" 对话框
+// // window.find();
 
-// try {
-//   var baiduWin = window.open('http://www.baidu.com', '_blank');
+// function getQueryStringArgs () {
+//   // 取得查询字符串并去掉开头的问号
+//   var qs = (location.search.length > 0 ? location.search.substring(1) : '');
 
-//   if (baiduWin == null) {
-//     blocked = true;
+//   // 保存数据的对象
+//   var args = {};
+
+//   // 取得每一项
+//   var items = qs.length ? qs.split('&') : [];
+//   var item = null;
+//   var name = null;
+//   var value = null;
+
+//   // 在for循环中使用
+//   var i = 0;
+//   var len = items.length;
+
+//   // 逐个将每一项添加到args对象中
+//   for (i = 0; i < len; i++) {
+//     item = items[i].split('=');
+//     name = decodeURIComponent(item[0]);
+//     value = decodeURIComponent(item[1]);
+
+//     if (name.length) {
+//       args[name] = value;
+//     }
 //   }
-// } catch (ex) {
-//   blocked = true;
+
+//   return args;
 // }
 
-// if (blocked) {
-//   console.log('The popup was blocked!');
-// }
+// // 假设查询字符串是?q=javascript&num=10
 
-// // 设置超时调用
-// var timeoutId = setTimeout(() => {
-//   alert('hell!');
-// }, 1000);
+// var args = getQueryStringArgs();
 
-// // 取消
-// clearTimeout(timeoutId);
+// console.log(args["q"]); // "javascript"
+// console.log(args["num"]); // "10"
 
-// // 显示 "打印" 对话框
-// window.print();
+// location.assign('http://www.baidu.com');
 
-// // 显示 "查找" 对话框
-// window.find();
-
-function getQueryStringArgs () {
-  // 取得查询字符串并去掉开头的问号
-  var qs = (location.search.length > 0 ? location.search.substring(1) : '');
-
-  // 保存数据的对象
-  var args = {};
-
-  // 取得每一项
-  var items = qs.length ? qs.split('&') : [];
-  var item = null;
-  var name = null;
-  var value = null;
-
-  // 在for循环中使用
-  var i = 0;
-  var len = items.length;
-
-  // 逐个将每一项添加到args对象中
-  for (i = 0; i < len; i++) {
-    item = items[i].split('=');
-    name = decodeURIComponent(item[0]);
-    value = decodeURIComponent(item[1]);
-
-    if (name.length) {
-      args[name] = value;
-    }
-  }
-
-  return args;
-}
-
-// 假设查询字符串是?q=javascript&num=10
-
-var args = getQueryStringArgs();
-
-console.log(args["q"]); // "javascript"
-console.log(args["num"]); // "10"
+window.location = 'http://www.baidu.com';
+location.href = 'http://www.baidu.com';
