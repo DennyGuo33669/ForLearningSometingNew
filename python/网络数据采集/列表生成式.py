f9c60bb1ad799a3d5ed8d4e7bd3fe4b5d813680e
DennyGuo_PC <dennyguo33669@gmail.com>
2020-09-03 00:15:26 +0800

    更新

--- python/网络数据采集/列表生成式.py
+++ python/网络数据采集/列表生成式.py
@@ -1,11 +1,32 @@
+import os
 # print(list(range(1, 101)))
 
 
-print([x * x for x in range(1, 11)])
-# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# print([x * x for x in range(1, 11)])
+# # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
-print([x * x for x in range(1, 11) if x % 2 == 0])
-# [4, 16, 36, 64, 100]
+# print([x * x for x in range(1, 11) if x % 2 == 0])
+# # [4, 16, 36, 64, 100]
 
-print([m + n for m in 'ABC' for n in 'XYZ'])
-# ['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+# print([m + n for m in 'ABC' for n in 'XYZ'])
+# # ['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+
+# print([item for item in os.listdir('D:\视频')])
+
+
+# d = {'x': 'A', 'y': 'B', 'z': 'C'}
+# for k, v in d.items():
+#     print(k, '=', v)
+# # x = A
+# # y = B
+# # z = C
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [item.lower() for item in L1 if isinstance(item, str) == True]
+# 测试:
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
