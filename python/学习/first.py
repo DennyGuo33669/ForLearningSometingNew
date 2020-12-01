9713406edf8f53d95153165637adb6557f02f7b7
DennyGuo_PC <dennyguo33669@gmail.com>
2020-12-01 23:43:52 +0800

    分支和格式输出

---
+++ python/学习/first.py
@@ -0,0 +1,12 @@
+# prince = 1000000
+# has_good_credit = True
+
+# if has_good_credit:
+#     down_payment = 0.1 * prince
+# else:
+#     down_payment = 0.2 * prince
+
+# print(f" Down payment:{down_payment}")
+
+
+# *****************************************************
